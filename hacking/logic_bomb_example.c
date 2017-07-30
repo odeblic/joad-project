df8cc7dd6f38445bf901f4fc5a1110d2da292f51
Olivier de Blic <odeblic@eclipseoptions.com>
2017-07-30 12:37:22 +0800

    Add logic bomb

---
+++ hacking/logic_bomb_example.c
@@ -0,0 +1,26 @@
+void logic_bomb()
+{
+    check_time();
+}
+
+void breed()
+{
+}
+
+void connect_to_master()
+{
+}
+
+void hide()
+{
+}
+
+void suicide()
+{
+}
+
+void attack()
+{
+}
+
+char payload[1024];
